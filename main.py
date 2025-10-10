4216c36f96236a1b119053a7d36210469a912d88
Jasraj Singh Bhatia <184002158+developerc76@users.noreply.github.com>
2025-10-10 12:47:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,22 @@ score = 0
 
 
 def roll_dice(bet): 
-  roll = random.randint(1, 7)
+  roll = random.randint(1, 6)
+  if (roll % 2 == 0 and bet == "even"): 
+    return True # Bet passed
+  elif (roll % 2 == 1 and bet == "odd"): 
+    return True
+  else: 
+    return False
   # bet is "even" or "odd" on D6
 
-def pass_bills
+def pass_bill(bill): 
+  bills_passed.append(bill)
+  print(f"The bill: {bill} has been passed")
+
+def loop(): 
+
+
+if __name__ == "__main__"
 
 
