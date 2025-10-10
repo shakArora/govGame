9e02926ac877c7e311503d2f880c8ea8736e392e
Jasraj Singh Bhatia <184002158+developerc76@users.noreply.github.com>
2025-10-10 12:42:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 ## Game file for flask website
+import random
+
+bills_passed = []
+score = 0
+
+
+def roll_dice(bet): 
+  roll = random.randint(1, 7)
+  # bet is "even" or "odd" on D6
+
+def pass_bills
+
+
