40f2c5ef2f2dd3027c6d52324ae7306cf943a62e
Shivum Arora <arorashivum@gmail.com>
2025-10-13 18:13:29 -0400

    gh pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 export default defineConfig({
+  base: '/govGame/',
   plugins: [react()],
   server: {
     port: 5173
